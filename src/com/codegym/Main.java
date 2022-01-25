7423239efbd92406498681a576d864f1a966e1f1
ruukichi <ngovietlc93@gmail.com>
2022-01-25 17:35:32 +0700

    add

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,57 @@
+package com.codegym;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String YES = "Yes";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập tên file nguồn: ");
+        String inputFileName = sc.nextLine();
+
+        if (!fileExist(inputFileName)) {
+            System.out.println("File không tồn tại!");
+        } else {
+            System.out.print("Nhập tên file đích: ");
+            String outputFileName = sc.nextLine();
+
+            String confirm = YES;
+            if (fileExist(outputFileName)) {
+                System.out.print("File đã tồn tại. Bạn có muốn ghi đè file này <Yes/No>");
+                confirm = sc.next().toLowerCase();
+            }
+
+            if (confirm.equals(YES)) {
+                copyFile(inputFileName, outputFileName);
+            }
+        }
+    }
+
+    public static boolean fileExist(String fileName) {
+        File file = new File(fileName);
+        return file.isFile();
+    }
+
+    public static void copyFile(String inputFileName, String outputFileName) {
+        try {
+            FileReader fileReader = new FileReader(inputFileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter(outputFileName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(line + "\n");
+            }
+
+            bufferedReader.close();
+            fileReader.close();
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
